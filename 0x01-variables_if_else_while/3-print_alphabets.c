cde49eb34d6d3a8185fd065b028d87d786ea744f
msDee060 <dananamercy@gmail.com>
2023-09-08 04:49:26 -0700

    Alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i, j;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	for (j = 'A'; j <= 'Z'; j++)
+		putchar(j);
+	putchar('\n');
+
+	return (0);
+}
+
