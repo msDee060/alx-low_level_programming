068af7bdd602a8fdc7b95704c8636132ccc278be
msDee060 <dananamercy@gmail.com>
2023-09-09 23:17:37 -0700

    Variables-if-else-while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,18 +9,17 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 99; i++)
+	for (i = 0; i <= 98; i++)
 	{
 		for (j = i + 1; j <= 99; j++)
 		{
-			putchar('0' + i / 10);
-			putchar('0' + i % 10);
-
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
 			putchar(' ');
-
-			putchar('0' + j / 10);
-			putchar('0' + j % 10);
-
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (i == 98 && j == 99)
+				continue;
 			putchar(',');
 			putchar(' ');
 		}
