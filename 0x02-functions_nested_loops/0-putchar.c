f27acfe66728c437bc50d7a8f0a8bbff7f45295a
msDee060 <dananamercy@gmail.com>
2023-09-12 09:44:55 -0700

    main

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,11 +5,16 @@
  *
  * Return: Always 0.
  */
-
-int _putchar(char c);
 int main(void)
 {
-	int _putchar();
-	putchar('\n);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
