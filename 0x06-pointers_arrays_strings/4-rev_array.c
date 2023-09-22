125ffda8fc9e2a2dd465a51aa7b52778857c471d
msDee060 <dananamercy@gmail.com>
2023-09-21 19:42:55 -0700

    Task 4 and task 7

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,7 +2,7 @@
 
 /**
   * reverse_array - reverses the content of an array of integers.
-  * @a: an array of integers. 
+  * @a: an array of integers.
   * @n: number of elements to swap.
   * Return: void.
   */
