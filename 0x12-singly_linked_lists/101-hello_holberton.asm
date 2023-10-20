142db962b703b188e0e532ababa7408f1cd0646e
msDee060 <dananamercy@gmail.com>
2023-10-20 04:41:19 -0700

    Upadate on assembly code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,10 +1,14 @@
 global main
 	extern printf
+
+section .data
+format: db 'Hello, Holberton', 0xa, 0
+
+section .text
 main:
 	mov edi, format
 	xor eax, eax
 	call printf
-	mov	eax, 0
-	ret
-format: db 'Hello, Holberton', 0
 
+	mov eax, 0
+	ret
