208f55f29a43969ef2771a741f9a884ab1d823a3
msDee060 <dananamercy@gmail.com>
2023-10-23 03:09:28 -0700

    Print times-table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,45 +3,37 @@
 /**
 * print_times_table - prints the n times table.
 * @n: number to be entered.
-*
-* Return: the times table.
 */
 void print_times_table(int n)
 {
-int i, j;	
-if (n < 0 || n > 15)
-{
-return;
-}
+	int digit, mul, res;
 
-for (i = 0; i <= n; i++)
-{
-for (j = 0; j <= n; j++)
-{
-int product = i * j;
-
-/* Print the product with appropriate formatting*/
-if (j == 0)
-{
-_putchar('0' + product);
-}
-else
-{
-_putchar(',');
-_putchar(' ');
-
-if (product < 10)
-{
-_putchar(' ');
-}
-else
-{
-_putchar('0' + (product / 10));
-}
-
-_putchar('0' + (product % 10));
-}
-}
-_putchar('\n');
-}
+	if (n >= 0 && n <= 15)
+	{
+	for (digit = 0; digit <= n; digit++)
+	{
+	_putchar('0');
+		for (mul = 1; mul <= n; mul++)
+		{
+		_putchar(',');
+		_putchar(' ');
+		res = digit * mul;
+		if (res <= 99)
+			_putchar(' ');
+		if (res <= 9)
+			_putchar(' ');
+		if (res >= 100)
+		{
+			_putchar((res / 100) + '0');
+			_putchar(((res / 10) % 10) + '0');
+		}
+		else if (res <= 99 && res >= 10)
+		{
+			_putchar((res / 10) + '0');
+		}
+		_putchar((res % 10) + '0');
+		}
+		_putchar('\n');
+	}
+	}
 }
